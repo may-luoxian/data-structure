3f1f823c18c3a26f589be5bbc9a4e6ca71ff1368
sunyk <2876053639@qq.com>
2024-08-16 22:09:47 +0800

    new function: new stack application

--- data-structure/main.c
+++ data-structure/main.c
@@ -1,14 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define ARRAY_LENGTH(arr) (sizeof(arr) / sizeof(arr[0]))
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include "data-type.h"
 
 int main() {
-	SeqStack S;
-	InitSeqStack(&S);
-	Push(&S, 1);
-
-	int x = GetTop(S);
-	printf("%d", x);
+	char str[] = {'(','q', '(', ')', ')'};
+	bool result = bracketCheck(&str, ARRAY_LENGTH(str));
+	printf("%d", result);
 }
--- data-structure/stack.c
+++ data-structure/stack.c
@@ -42,3 +42,47 @@ int GetTop(SeqStack S) {
 	}
 	return S.data[S.top];
 }
+
+// 判断栈空
+bool isEmptySeqStack(SeqStack S) {
+	return S.top == -1;
+}
+
+
+// 栈的应用
+// 括号匹配的应用
+bool bracketCheck(char* str, int length) {
+	// 初始化栈
+	SeqStack S;
+	InitSeqStack(&S);
+
+	// 循环遍历括号数组
+	for (int i = 0; i < length; i++) {
+		// 判断字符是否符合规范
+		if (str[i] != '(' && str[i] != '[' && str[i] != '{' && str[i] != ")" && str[i] != "]" && str[i] != "}") {
+			printf("字符不符合规范");
+			return false;
+		}
+
+		if (str[i] == '(' || str[i] == '[' || str[i] == '{') { // 如果是左括号，入栈
+			Push(&S, str[i]);
+		}
+		else { // 如果是右括号，则需要从栈中寻找与之匹配的左括号。
+			// 判断栈是否为空，为空直接匹配失败
+			if (isEmptySeqStack(S)) {
+				return false;
+			}
+			char topElem = Pop(&S);
+			if (str[i] == ")" && topElem != "(") {
+				return false;
+			}
+			if (str[i] == "]" && topElem != "[") {
+				return false;
+			}
+			if (str[i] == "}" && topElem != "{") {
+				return false;
+			}
+		}
+	}
+	return isEmptySeqStack(S); // 最终判断栈是否为空
+}
